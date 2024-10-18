7b0c4a700824c1f7db0cce203a11c92d259c9bac
tlstoddk99 <tlstoddk99@gmail.com>
2024-10-18 11:01:03 +0900

    fix distance base_link to laser

--- f1tenth_stack/launch/bringup_launch.py
+++ f1tenth_stack/launch/bringup_launch.py
@@ -136,7 +136,8 @@ def generate_launch_description():
         package='tf2_ros',
         executable='static_transform_publisher',
         name='static_baselink_to_laser',
-        arguments=['0.27', '0.0', '0.11', '0.0', '0.0', '0.0', 'base_link', 'laser']
+        # arguments=['0.27', '0.0', '0.11', '0.0', '0.0', '0.0', 'base_link', 'laser']
+        arguments=['0.11', '0', '0', '0', '0', '0', '1', 'base_link', 'laser'],
     )
     f1tenth_mux_node = Node(
         package='f1tenth_stack',
